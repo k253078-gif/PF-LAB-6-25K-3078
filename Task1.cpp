56d2120165047e8233ec4fb0f1165fe7ae83c053
Muhammad Wasi Bin Tariq <k253078@nu.edu.pk>
2025-10-05 23:27:15 +0500

    Added justification for using while loop

--- Task1.cpp
+++ Task1.cpp
@@ -3,9 +3,10 @@
 int main() {
     int num, sum = 0;
 
-    printf("Enter numbers (0 to stop):\n");
+    printf("Enter numbers (0 to stop):\n"); 
 
-    while (1) {  
+    while (1) {  /*Here while loop is used instead of for as we do not 
+		           know how many times the loop will execute*/
         
         if (scanf("%d", &num)== 0 || num == 0 ) 
 		break;  
@@ -16,4 +17,5 @@ int main() {
     printf("Final sum = %d\n", sum);
     return 0;
 }
+
 
